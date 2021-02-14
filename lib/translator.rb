c2a38b13ad3cc1b37a1700475924f0322788bf4f
Nathaniel Martinez <nathanielmartinez2001@gmail.com>
2021-02-14 21:37:45 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -14,7 +14,7 @@ def get_japanese_emoticon(path, emoticon)
   
   load_library(path).each do |key, value|
     if value[:english] == emoticon
-      return value
+      return value[:japanese]
 
 end
 
